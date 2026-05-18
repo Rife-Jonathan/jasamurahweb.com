ff7d4f748f9b7ead6ec6952851237e33b885c337
rife <rifeni3@gmail.com>
2026-05-18 14:21:34 +0700

    fix: use www as canonical URL + add non-www redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://jasamurahweb.com',
+  site: 'https://www.jasamurahweb.com',
   integrations: [mdx(), sitemap()],
 });
